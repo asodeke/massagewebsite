5dbd72c78cf4867c197b8c3191861bcc4e0e94f2
Ayo Sodeke <ayo.sodeke@gmail.com>
2018-10-17 08:28:40 -0500

    Added install and fetch events
    Added install event for the service worker also added the files to cache and fetch event.

---
+++ sw.js
@@ -0,0 +1,38 @@
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/',
+  '/js/bootstrap.js',
+  '/js/bootstrap.min.js',
+  '/js/jquery.min.js',
+  '/img/'
+];
+
+//Install service worker
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+//cache and return request
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
